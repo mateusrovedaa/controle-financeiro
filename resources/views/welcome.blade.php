754251ecb900258d80c2155a9e62ff0d8f0825f7
Mateus <mateus@aztecweb.net>
2021-06-11 18:02:51 -0300

    Ajusta pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                GCS - Financial Control
+                GCS
             </div>
         </div>
     </div>
