9393088099afe5f1b72c52955fef1c9181b1ad43
mateusrovedaa <mateus_roveda@hotmail.com>
2021-06-11 21:59:44 -0300

    Ajusta pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,7 +85,7 @@
 
         <div class="content">
             <div class="title m-b-md">
-                GCS
+                GCS - Financial Control
             </div>
         </div>
     </div>
